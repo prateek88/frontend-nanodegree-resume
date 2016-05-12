e46dee12391461a695e8ed2279eed974c383d5aa
prateek goyal <prateek.goyal7434@gmail.com>
2016-05-12 13:13:58 +0530

    prepend formatted name and role in header

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,24 @@
+//Code started on 10 may 2016
 /*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var firstName = "Prateek";
+var age = 28;
+
+console.log(firstName);
+
+var awesomeThoughts = "I am Prateek and I am AWESOME";
+$("#main").append(funThoughts);
+
+var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
+//console.log(funThoughts);
+$("#main").append(funThoughts);
+*/
+
+var name = "Prateek Goyal";
+
+var role = "Web Developer";
+
+var formattedName = HTMLheaderName.replace("%data%", name );
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
